a95e20137f187b1392655807956232138ed90533
Edutommy <emau.emv@gmail.com>
2020-09-28 00:43:29 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,59 @@
+const fila = document.querySelector('.contenedor-carusel');
+const peliculas = document.querySelectorAll('.pelicula');
+
+const flechaizq = document.getElementById('flecha-izq');
+const flechader = document.getElementById('flecha-der');
+
+//DERECHA
+flechader.addEventListener('click', () =>{
+    fila.scrollLeft += fila.offsetWidth;
+
+    const indicadorActivo = document.querySelector('.indicadores .activo');
+    if(indicadorActivo.nextSibling){
+        indicadorActivo.nextSibling.classList.add('activo');
+        indicadorActivo.classList.remove('activo');
+    }
+});
+//IZQUIERDA
+flechaizq.addEventListener('click', () =>{
+    fila.scrollLeft -= fila.offsetWidth;
+
+    const indicadorActivo = document.querySelector('.indicadores .activo');
+    if(indicadorActivo.previousSibling){
+        indicadorActivo.previousSibling.classList.add('activo');
+        indicadorActivo.classList.remove('activo');
+    }
+});
+
+
+const nPaginas = Math.ceil(peliculas.length / 5);
+for(let i = 0; i < nPaginas; i++){
+    const indicador = document.createElement('button');
+
+    if(i === 0){
+        indicador.classList.add('activo');
+    }
+
+    document.querySelector('.indicadores').appendChild(indicador);
+    indicador.addEventListener('click', (e) => {
+        fila.scrollLeft = i * fila.offsetWidth;
+
+        document.querySelector('.indicadores .activo').classList.remove('activo');
+        e.target.classList.add('activo');
+    });
+}
+
+//HOVER
+peliculas.forEach((pelicula) => {
+    pelicula.addEventListener('mouseenter', (e) =>{
+        const elemento = e.currentTarget;
+        setTimeout(() => {
+            peliculas.forEach(pelicula => pelicula.classList.remove('hover'));
+            elemento.classList.add('hover');
+        }, 300);
+    });
+});
+
+fila.addEventListener('mouseleave', () => {
+    peliculas.forEach(pelicula => pelicula.classList.remove('hover'));
+});
